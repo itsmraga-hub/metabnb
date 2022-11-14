df209ce2a8397004dd8070820bc2c756b08cf748
Raga <ragawilliam570@gmail.com>
2022-11-14 17:53:08 +0300

    Add Navbar to Place To Stay page

--- src/pages/PlaceToStay.jsx
+++ src/pages/PlaceToStay.jsx
@@ -1,10 +1,12 @@
 import Footer from "../components/Footer";
 import Header from "../components/Header";
+import Navbar from "../components/Navbar";
 import PlaceToStayListDisplay from "../components/PlaceToStayListDisplay";
 
 const PlaceToStay = () => (
   <>
     <Header />
+    <Navbar />
     <main>
       <PlaceToStayListDisplay />
     </main>
