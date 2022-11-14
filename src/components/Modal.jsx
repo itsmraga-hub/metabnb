fca3e61671cef4e9412a8fb920a77703705d7cae
Raga <ragawilliam570@gmail.com>
2022-11-14 18:25:16 +0300

    Finish modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import style from "./css/Modal.module.css";
 import { RiCloseLine } from "react-icons/ri";
+import { BiChevronRight } from "react-icons/bi";
+
+import Metamask from '../assets/metamask-yellow.svg';
+import WalletConnect from '../assets/walletconnect.svg';
 
 const Modal = ({ setIsOpen }) => {
   return (
@@ -14,10 +18,16 @@ const Modal = ({ setIsOpen }) => {
                 <RiCloseLine style={{width: '30px', height: '30px'}} />
               </button>
             </div>
-            <div>
+            <div className={style.Body}>
               <p>Choose your preferred wallet:</p>
-              <button></button>
-              <button></button>
+              <button>
+                <img src={Metamask} alt="metamask" />
+                <BiChevronRight style={{width: '30px', height: '30px'}} />
+              </button>
+              <button>
+                <img src={WalletConnect} alt="Wallet connect" />
+                <BiChevronRight style={{width: '30px', height: '30px'}} />
+              </button>
             </div>
           </div>
         </div>
